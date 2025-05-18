61344310032d78465f74dbd429c8e9c44a5e5b8d
donk <dkunz2024>
2025-05-17 19:39:03 -0700

    no idea if this is correct

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 library(scales)
+library(shinyBS)
 
 ds <- read_csv("https://raw.githubusercontent.com/dkunz2024/ds502final/master/pokemon_ds.csv")
 ds <- ds %>% 
@@ -13,8 +14,10 @@ ds <- ds %>%
          Body_Style = as.factor(Body_Style))
 
 types <- read_csv("https://raw.githubusercontent.com/dkunz2024/ds502final/master/pokemon_type_ds.csv")
+pokemon_types <- types
 types <- types %>% 
   rename("Type 1"=...1)
+type_list <- types$`Type 1`
 types <- types %>% 
   pivot_longer(
     cols=2:19,
@@ -22,29 +25,81 @@ types <- types %>%
     values_to = "Effectiveness"
   )
 
-print(str(ds))
-print(str(types))
+#given a move and two types, reports the effectiveness of using that move 
+f.calc_effectiveness <- function(move, type1, type2=NA){
+  effectiveness <- 1
+  effectiveness = effectiveness * types[types$`Type 1`==move & types$`Type 2`==type1, "Effectiveness"] %>% pull("Effectiveness")
+  if(!is.na(type2)){
+    effectiveness = effectiveness * types[types$`Type 1`==move & types$`Type 2`==type2, "Effectiveness"] %>% pull("Effectiveness")
+  }
+  effectiveness
+}
+
+#calculates the maximum effectiveness four moves could have against a pokemon
+f.max_effectiveness <- function(moves, type1, type2=NA){
+  myvec <- numeric(0)
+  for (i_move in moves){
+    myvec <- c(myvec, f.calc_effectiveness(i_move, type1, type2))
+  }
+  c(moves[which.max(myvec)], max(myvec))
+}
+
+# Kaiden data 
+pokemon_types = pokemon_types %>% 
+  pivot_longer(
+    cols = Normal:Fairy,
+    names_to = "Defending_Type", 
+    values_to = "Effectiveness"
+  ) %>% 
+  rename("Attacking_Type" = ...1)
+
+
+pokemon_type_names = c("Bug", "Dark", "Dragon", "Electric", "Fairy", "Fighting",
+                       "Fire", "Flying", "Ghost", "Grass", "Ground", 
+                       "Ice", "Normal", "Poison", "Psychic", "Rock", "Steel",
+                       "Water")
+
+pokemon_type_colors = c("#77820f", "#514343", "#232f98", 
+                        "#c59b00", "#801e86", "#c66a00", 
+                        "#a9220f", "#214a7c", "#5b3a5e", 
+                        "#438225", "#73461e", "#3186a6", 
+                        "#53585b", "#6a339c", "#9b1e40", 
+                        "#545038", "#467285", "#2559b2")
+
 
 # Define UI 
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Gapminder: Simple Shiny Demo"),
+  titlePanel("Pokemon Battle Assistant"),
   
-  # Sidebar with a slider input the year 
+  # Sidebar to select 2 pokemon  
   sidebarLayout(
     sidebarPanel(
       textInput("p1", 
-                  h4("First Pokemon"),
-                  value = "Pikachu"),
+                h4("Attacking Pokemon"),
+                value = "Pikachu"),
+      bsCollapse(
+        id = "move_types_panel",
+        open = "Panel 1",  # Initial open panel
+        bsCollapsePanel(
+          title = "Move Types",
+          selectInput("move1", "", type_list, selected = "Normal"),
+          selectInput("move2", "", type_list, selected = "Normal"),
+          selectInput("move3", "", type_list, selected = "Normal"),
+          selectInput("move4", "", type_list, selected = "Normal"),
+          style = "primary"
+        )
+      ),
       textInput("p2", 
-                  h4("Second Pokemon"), 
-                  value = "Charmander")
+                h4("Defending Pokemon"), 
+                value = "Charmander")
     ),
     
     # Show the plots
     mainPanel(
       plotOutput("radarPlot"), 
+      plotOutput("typesPlot")
     )
   )
 )
@@ -56,6 +111,16 @@ server <- function(input, output) {
     
     p1 <- input$p1
     p2 <- input$p2
+    
+    #Getting type colors
+    p1_type <-  ds %>% 
+      filter(Name==p1) %>% 
+      pull(Type_1)
+    p2_type <-  ds %>% 
+      filter(Name==p2) %>% 
+      pull(Type_1)
+    p1_color <- pokemon_type_colors[which(pokemon_type_names == p1_type)]
+    p2_color <- pokemon_type_colors[which(pokemon_type_names == p2_type)]
     
     #Create dataset of the two pokemon
     dsc <- ds %>%
@@ -69,7 +134,7 @@ server <- function(input, output) {
       ) %>%
       mutate(Stat_Type=factor(Stat_Type, levels=c("HP", "Attack", "Defense", "Speed", "Sp_Def", "Sp_Atk"))) %>%
       arrange(Number)
-
+    
     #Create the plot
     dsc %>%
       ggplot(aes(x=Stat_Type, y=Stat_Value))+
@@ -86,6 +151,56 @@ server <- function(input, output) {
         strip.text.x = element_text(size=10, face="bold")
       )
   })
+  
+  output$typesPlot <- renderPlot({
+    # generate plot based on input$country from ui.R
+    
+    # define variables from inputs
+    if (is.null(input$move_types_panel)){
+      attackingType <- ds %>% 
+        filter(Name==input$p1) %>% 
+        pull(Type_1) %>% 
+        as.character()
+      defendingType <- c(
+        as.character(ds[ds$Name==input$p2, "Type_1"] %>% pull("Type_1")),
+        as.character(ds[ds$Name==input$p2, "Type_2"] %>% pull("Type_2"))
+      )
+    } else {
+      attackingType <- c(input$move1, input$move2, input$move3, input$move4)
+      defendingType <- c(
+        as.character(ds[ds$Name==input$p2, "Type_1"] %>% pull("Type_1")),
+        as.character(ds[ds$Name==input$p2, "Type_2"] %>% pull("Type_2"))
+      )
+    }
+    
+    # filter
+    attacking = pokemon_types %>%
+      filter(Attacking_Type %in% attackingType)
+    defending = pokemon_types %>%
+      filter(Defending_Type %in% defendingType)
+    both_types = pokemon_types %>%
+      filter(Attacking_Type %in% attackingType) %>%
+      filter(Defending_Type %in% defendingType)
+    
+    p = ggplot(pokemon_types) +
+      geom_tile(aes(x=Attacking_Type, y=Defending_Type, fill=Effectiveness), color="black", alpha=0.3) +
+      geom_tile(data=attacking, aes(x=Attacking_Type, y=Defending_Type, fill=Effectiveness), color="black", linewidth=0.5, alpha=0.5) +
+      geom_tile(data=defending, aes(x=Attacking_Type, y=Defending_Type, fill=Effectiveness), color="black", linewidth=0.5, alpha=0.5) +
+      geom_tile(data=both_types, aes(x=Attacking_Type, y=Defending_Type, fill=Effectiveness), color="black", linewidth=0.5) +
+      scale_fill_gradientn(colors=c("black", "red","white", "white", "green"))+
+      labs(
+        x = "Attacking Type",
+        y = "Defending Type",
+        caption = "Source: Kaggle"
+      ) +
+      theme(axis.text.x = element_text(size=8), # legible axis labels (at least not overlapping)
+            axis.text.y = element_text(size=8)
+      )
+    
+    p
+    
+  })
+  
 }
 
 # Run the application 
